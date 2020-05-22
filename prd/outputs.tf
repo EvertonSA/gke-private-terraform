693570118d4ad21bb5b532fd62815ac1402f8ab2
Everton Seiei Arakaki <30481051+EvertonSA@users.noreply.github.com>
2020-05-22 16:44:13 -0300

    Update outputs.tf

--- prd/outputs.tf
+++ prd/outputs.tf
@@ -44,10 +44,6 @@ output "get_credentials" {
   description = "Gcloud get-credentials command"
   value       = format("gcloud container clusters get-credentials --project %s --region %s --internal-ip %s", var.project, var.region, var.cluster_name)
 }
-output "bastion_ssh" {
-  description = "Gcloud compute ssh to the bastion host command"
-  value       = format("gcloud compute ssh %s --project %s --zone %s -- -L8888:127.0.0.1:8888", google_compute_instance.bastion.name, var.project, google_compute_instance.bastion.zone)
-}
 
 output "bastion_kubectl" {
   description = "kubectl command using the local proxy once the bastion_ssh command is running"
